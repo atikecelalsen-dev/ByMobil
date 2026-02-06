75de7b95c1af30ddbf520685ca6ef9c3d1899417
Atike <hatikecelalsen@gmail.com>
2026-02-06 08:49:15 +0300

    ..

---
+++ ByMobil/Controllers/SecondController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BayMobil.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Anasayfa() { return View(); }
+        public IActionResult CariArama() { return View(); }
+        public IActionResult CariDetay() { return View(); }
+        public IActionResult SiparisListe() { return View(); }
+        public IActionResult SiparisDuzenle() { return View(); }
+
+
+
+    }
+}
